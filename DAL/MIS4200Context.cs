e31874f6015f006360005dd37b62ac45a0972775
Nicholas Hegerty <nh645015@ohio.edu>
2020-01-29 10:32:57 -0500

    added a controller as well as added a context page and also an order page

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using NH645015_MIS4200.Models;
+
+namespace NH645015_MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+            // this method is a 'constructor' and is called when a new context is created
+            // the base attribute says which connection string to use
+        }
+
+        // Include each object here. The value inside <> is the name of the class,
+        // the value outside should generally be the plural of the class name
+        // and is the name used to reference the entity in code
+
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<customer> Customers { get; set; }
+
+    }
+}
